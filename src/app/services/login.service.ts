78e65cccdcc2809af45ef36587ea8490bb93aada
paulo <pgsilva142@gmail.com>
2019-06-15 01:31:36 -0300

    Login

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, http } from '@angular/core';
+import { API_URL } from './config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { 
+    
+  }
+
+  doLogin(){
+  }
+}
