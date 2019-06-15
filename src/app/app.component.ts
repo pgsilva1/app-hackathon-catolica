39f2d96a6db8e255ce5f84d6086a630fb2635dfd
paulo <pgsilva142@gmail.com>
2019-06-15 07:30:19 -0300

    Retificações!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,22 @@
 import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { Platform }     from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar }    from '@ionic-native/status-bar/ngx';
-import { Router }       from '@angular/router';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-    selector   : 'app-root',
-    templateUrl: 'app.component.html'
+  templateUrl: 'app.html'
 })
-export class AppComponent {
-    constructor(
-        private platform: Platform,
-        private splashScreen: SplashScreen,
-        private statusBar: StatusBar,
-        private router: Router
-    ) {
-        this.initializeApp();
-    }
+export class MyApp {
+  rootPage:any = TabsPage;
 
-    initializeApp() {
-        this.platform.ready().then(() => {
-            this.statusBar.styleDefault();
-            this.splashScreen.hide();
-
-            const user = localStorage.getItem('user');
-            if (!user) {
-                this.router.navigate(['/login']);
-            }
-        });
-    }
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,43 @@
-import { NgModule }           from '@angular/core';
-import { BrowserModule }      from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { ChartsModule } from 'ng2-charts-x';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen }                    from '@ionic-native/splash-screen/ngx';
-import { StatusBar }                       from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }     from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-    declarations   : [AppComponent],
-    entryComponents: [],
-    imports        : [
-        BrowserModule,
-        AppRoutingModule,
-        IonicModule.forRoot(),
-    ],
-    providers      : [
-        StatusBar,
-        SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-    ],
-    bootstrap      : [AppComponent]
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    ChartsModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
-export class AppModule {
-}
+export class AppModule {}
