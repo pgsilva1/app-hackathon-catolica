0702360f7218b052c6f9f84c299489a5d5e82cc2
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2019-06-15 04:45:56 -0300

    tela de login

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component }           from '@angular/core';
+import { ChartType }           from 'chart.js';
+import { Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector   : 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls  : ['tab1.page.scss']
 })
 export class Tab1Page {
 
-  constructor() {}
+    public doughnutChartLabels: Label[]    = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+    public doughnutChartData: MultiDataSet = [
+        [350, 450, 100],
+        [50, 150, 120],
+        [250, 130, 70],
+    ];
+    public doughnutChartType: ChartType    = 'doughnut';
+
+    constructor() {
+    }
 
 }
